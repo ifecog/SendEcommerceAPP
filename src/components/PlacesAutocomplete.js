f5c496a0660191cc4a0ef2a6a81187c32f8215ce
progIfe <ifeoluwasamson90@gmail.com>
2024-07-23 10:47:24 +0100

    Fix security issue

--- src/components/PlacesAutocomplete.js
+++ src/components/PlacesAutocomplete.js
@@ -1,39 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { LoadScript, Autocomplete } from '@react-google-maps/api';
+import React, {useState, useEffect, useRef} from 'react'
+import {LoadScript, Autocomplete} from '@react-google-maps/api'
 
-const PlacesAutocomplete = ({ value, onChange, onSelect }) => {
-  const [address, setAddress] = useState(value);
-  const autocompleteRef = useRef(null);
+const PlacesAutocomplete = ({value, onChange, onSelect}) => {
+  const [address, setAddress] = useState(value)
+  const autocompleteRef = useRef(null)
 
   const handlePlaceChanged = () => {
-    const place = autocompleteRef.current.getPlace();
+    const place = autocompleteRef.current.getPlace()
     if (place.geometry) {
-      const formattedAddress = place.formatted_address;
-      const lat = place.geometry.location.lat();
-      const lng = place.geometry.location.lng();
-      setAddress(formattedAddress);
+      const formattedAddress = place.formatted_address
+      const lat = place.geometry.location.lat()
+      const lng = place.geometry.location.lng()
+      setAddress(formattedAddress)
       onSelect({
         address: formattedAddress,
         latitude: lat,
         longitude: lng,
-      });
+      })
     }
-  };
+  }
 
   useEffect(() => {
     if (window.google) {
-      const autocomplete = new window.google.maps.places.Autocomplete(document.getElementById('address'), {
-        types: ['address'],
-        componentRestrictions: { country: 'ng' },
-      });
-      autocompleteRef.current = autocomplete;
-      autocomplete.addListener('place_changed', handlePlaceChanged);
+      const autocomplete = new window.google.maps.places.Autocomplete(
+        document.getElementById('address'),
+        {
+          types: ['address'],
+          componentRestrictions: {country: 'ng'},
+        }
+      )
+      autocompleteRef.current = autocomplete
+      autocomplete.addListener('place_changed', handlePlaceChanged)
     }
-  }, []);
+  }, [])
 
   return (
     <LoadScript
-      googleMapsApiKey='AIzaSyAV-Ro1WZ5bFKJsxEjgRn8ytNa9VxmEkrw'
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
       libraries={['places']}
     >
       <Autocomplete
@@ -45,15 +48,15 @@ const PlacesAutocomplete = ({ value, onChange, onSelect }) => {
           className='form-control'
           value={address}
           onChange={(e) => {
-            setAddress(e.target.value);
-            onChange(e.target.value);
+            setAddress(e.target.value)
+            onChange(e.target.value)
           }}
           placeholder='Enter address'
           id='address'
         />
       </Autocomplete>
     </LoadScript>
-  );
-};
+  )
+}
 
-export default PlacesAutocomplete;
+export default PlacesAutocomplete
